f95d690042e0b9b1a2a67b48268cd4d5b71304dd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-17 16:45:20 +0530

    updated

--- day1/dictionarydemo.py
+++ day1/dictionarydemo.py
@@ -16,12 +16,20 @@ for _ in range(1,3):
     area=fake.random_int(1000,10000)
     region=Region(region_code,region_name,poulation,area)
     regions.append(region)
-    for _ in range(1,4):
+    locations=[]
+    for i in range(1,4):
         location_code=fake.random_int(100,999)
         location_name=fake.city()
-        location=Location(location_code,location_name,region_code)
+        longitude=fake.longitude()
+        latitude=fake.latitude()
+        location=Location(location_code,location_name,longitude,latitude)
         locations.append(location)
         #key pair mapping
-        dict_region_location[region._id]=location
+    dict_region_location[region._id]=locations
 
 
+#print region code and location name
+for key,value in dict_region_location.items():
+    print(f"Region Code: {key})")
+    for loc in value:
+        print(f"    Location Name: {loc.get_location_name()}")
