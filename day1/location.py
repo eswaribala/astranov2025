5de929cd7fefe684a1615dd06c6d45f74264764b
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-17 14:45:26 +0530

    updated

--- day1/location.py
+++ day1/location.py
@@ -2,16 +2,25 @@
 class Location:
     # initialize the location with code, name, latitude, and longitude
     def __init__(self, location_code: int, location_name: str, latitude: float, longitude: float):
-        self.location_code = location_code
-        self.location_name = location_name
-        self.latitude = latitude
-        self.longitude = longitude
+        self._location_code = location_code
+        self._location_name = location_name
+        self._latitude = latitude
+        self._longitude = longitude
+
+    
 
     # getter and setter methods for location_name
     #assigning location name
+    def set_location_code(self, location_code: int):
+        self._location_code = location_code
+    
+    def get_location_code(self) -> int:
+        return self.location_code
+    
+    
     def set_location_name(self, location_name: str):
-        self.location_name = location_name
+        self._location_name = location_name
     
     def get_location_name(self) -> str:
-        return self.location_name
+        return self._location_name
     
--- day1/locationcontroller.py
+++ day1/locationcontroller.py
@@ -3,8 +3,35 @@ from location import Location
 from faker import Faker
 
 #faker instance
-fake =Faker("en_US")
+fake =Faker("en_IN")
 
+# generate fake location data for 100 locations
+
+locations = []
+for _ in range(1,100):
+    location_code = fake.unique.random_int(min=1000, max=9999)
+    location_name = fake.city()
+    latitude = float(fake.latitude())
+    longitude = float(fake.longitude())
+    location_object = Location(location_code, location_name, latitude, longitude)
+    locations.append(location_object)
+
+# print the location details
+
+for loc in locations:
+    print(f"Location Code: {loc._location_code}")
+    print(f"Location Name: {loc._location_name}")    
+    print(f"Latitude: {loc._latitude}")
+    print(f"Longitude: {loc._longitude}")
+    print("-----")
+
+
+
+
+
+
+
+"""
 #generate fake location data
 location_code = fake.unique.random_int(min=1000, max=9999)
 location_name = fake.city()
@@ -14,10 +41,10 @@ location_object=Location(location_code, location_name, latitude, longitude)
 
 
 #print the location details
-print(f"Location Code: {location_object.location_code}")
-print(f"Location Name: {location_object.location_name}")    
-print(f"Latitude: {location_object.latitude}")
-print(f"Longitude: {location_object.longitude}")
+print(f"Location Code: {location_object._location_code}")
+print(f"Location Name: {location_object._location_name}")    
+print(f"Latitude: {location_object._latitude}")
+print(f"Longitude: {location_object._longitude}")
 
 
 #update location name
@@ -26,3 +53,4 @@ location_object.set_location_name(new_location_name)
 
 #print the updated location name
 print(f"Updated Location Name: {location_object.get_location_name()}")
+"""
