27a48432fd57ca92b37969aaff9661658a3c9d7b
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-17 11:45:02 +0530

    updated

--- day1/identifiers.py
+++ day1/identifiers.py
@@ -8,6 +8,13 @@ Description: This module provides functions to generate or retrieve location det
 from faker import Faker
 fake =Faker("en_IN")
 location_code=fake.postcode()
+location_name=fake.city()
+location_coords=fake.local_latlng(country_code="IN", coords_only=True)
+
 
 print("Location Details:")
 print("Location Code:", location_code)
+print("Location Name:", location_name)
+print("Location Coordinates:", location_coords)
+
+print(f"Name: {location_name}, Code: {location_code}, Coordinates: {location_coords}")
