513d81d4e1ef42b4a33591cda0e319a2a74c54e7
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-18 09:29:14 +0530

    updated

--- day2/locationset.py
+++ day2/locationset.py
@@ -14,3 +14,14 @@ for _ in range(10):
 
 print("Location Set 1:", location_set_1)
 print("Location Set 2:", location_set_2)
+
+#union of two sets
+union_set = location_set_1.union(location_set_2)
+print("Union of Location Sets:", union_set)
+
+#intersection of two sets
+intersection_set = location_set_1.intersection(location_set_2)
+print("Intersection of Location Sets:", intersection_set)
+#difference of two sets
+difference_set = location_set_1.difference(location_set_2)
+print("Difference of Location Sets:", difference_set)
