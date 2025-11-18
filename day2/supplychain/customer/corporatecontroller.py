51ded5ec24d853ad958d0944a0d62836dd705fb1
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-18 11:41:55 +0530

    updated

---
+++ day2/supplychain/customer/corporatecontroller.py
@@ -0,0 +1,4 @@
+from supplychain.customer.models import CorporateCustomer
+def add_corporate_customer(customer_id, name, email, company_type, tax_id):
+    corporate=CorporateCustomer(customer_id, name, email, company_type, tax_id)
+    return corporate
---
+++ day2/supplychain/customer/individualcontroller.py
@@ -0,0 +1,5 @@
+from supplychain.customer.models import IndividualCustomer
+
+def add_individual_customer(customer_id, name, email, date_of_birth,gender):
+    individual=IndividualCustomer(customer_id, name, email, date_of_birth,gender)
+    return individual
