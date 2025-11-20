d7ab77a7d6f8ec7bde97455501f766d72f5b016c
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-20 11:56:37 +0530

    updated

--- day3/supplychainapp/src/App.js
+++ day3/supplychainapp/src/App.js
@@ -5,7 +5,7 @@ import AstraHome from './components/organisms/AstraHome/AstraHome';
 function App() {
   return (
     <div className="App">
-     <h1>Supply Chain App</h1>
+    
      <AstraHome />
     </div>
   );
---
+++ day3/supplychainapp/src/components/atoms/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './Title.css';
+
+const Title = () => (
+  <h1 class="Title">Supply Chain App</h1>
+);
+
+
+export default Title;
