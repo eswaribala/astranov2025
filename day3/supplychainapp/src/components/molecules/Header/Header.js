27b5ca830ab61d40b3bb12bc6e380b1d708f7fdf
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-20 11:58:57 +0530

    updated

--- day3/supplychainapp/src/components/molecules/Header/Header.js
+++ day3/supplychainapp/src/components/molecules/Header/Header.js
@@ -3,13 +3,19 @@ import AstraBanner from '../../atoms/Banner/Banner';
 import  './Header.css';
 import Logo from '../../atoms/Logo/Logo';
 import Timer from '../../atoms/Timer/Timer';
+import Title from '../../atoms/Title/Title';
 
 
 const Header = () => (
+  <div>
+   <div>
+     <Title/>
+    </div> 
   <div class="Header" data-testid="Header">
     <Logo/>
     <AstraBanner/>
     <Timer/>
+  </div>
   </div>
 );
 
